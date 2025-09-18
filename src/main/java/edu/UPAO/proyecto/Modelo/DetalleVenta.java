b8cc3730106036722e0aa30042d6b6e378a055c5
Fabri <jhoseps771@gmail.com>
2025-09-18 00:21:16 -0500

    Revert "Backend Simplon"
    This reverts commit 8ba2a8696cc54431b2c20536f7c449eb16c334d2.

--- src/main/java/edu/UPAO/proyecto/Modelo/DetalleVenta.java
+++ src/main/java/edu/UPAO/proyecto/Modelo/DetalleVenta.java
@@ -10,11 +10,11 @@ public class DetalleVenta {
     public DetalleVenta() {
     }
 
-    public DetalleVenta(int idDetalle, Producto producto, int cantidad, double precioUnitario, double subtotal) {
-        this.idDetalle = idDetalle;
-        this.producto = producto;
+    public DetalleVenta(int cantidad, int idDetalle, double precioUnitario, Producto producto, double subtotal) {
         this.cantidad = cantidad;
+        this.idDetalle = idDetalle;
         this.precioUnitario = precioUnitario;
+        this.producto = producto;
         this.subtotal = subtotal;
     }
 
